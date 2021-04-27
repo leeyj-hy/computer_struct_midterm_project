9f5e07e7d9c114be1eb8e3e04ccd93d328f34f0d
leeyj-hy <dldydwo1230@hanyang.ac.kr>
2021-04-27 22:38:55 +0900

    fact2

--- fact.s
+++ fact.s
@@ -11,6 +11,24 @@ main:
     addi t1, a0, 0		#load a0 to t1
     jal ra, factorial		#jump to factorial and link next text's address to ra
 
+    
+
+factorial:
+    addi sp, sp, -8		#push
+    sw t0, 0(sp)
+    sw t1, 4(sp)
+
+    addi t1, t1, -1		#make t3==t1-1 (t1 is equal to t0 and its n)
+    addi s0, x0, 1		#s0==1
+    beq t1, s0, exit		#if t1==1, exit
+    
+    jal factorial
+
+    lw t0, 0(sp)		#pop
+    lw t1, 4(sp)
+    addi sp, sp, 8
+    
+exit:
     addi a1, a0, 0
     addi a0, x0, 1
     ecall 			# Print Result
@@ -22,10 +40,14 @@ main:
     addi a0, x0, 10
     ecall 			# Exit
 
-factorial:
-    beq t1, x0, main		#if t1==0, exit
+    
+
+
+
+
+
     addi t2, t1, -1		#make t2==t1-1 (t1 is equal to t0 and its n)
     
-    mul t3, t1, factorial		#save t1*t2 to t3
+    mul t3, t1, factorial	#save t1*t2 to t3
     add t1, x0, t2		#make t1=t2
     jal t3, factorial		#return t3 and link to factorial
